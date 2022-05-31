c8e7e8560eb42e3d0671d74cfe3ca5361f44051a
ChethanR2711 <chethanr2711@gmail.com>
2022-06-01 00:24:03 +0530

    empty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Login from './Components/Login/Login';
+// import Home from './Components/Home/Home';
 
 function App() {
   return (
