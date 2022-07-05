d882384b7df8b5ceed12520c9b6980b1bcbaa419
ChethanR2711 <chethanr2711@gmail.com>
2022-07-05 12:02:13 +0530

    simply

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Post from '../Post/Post';
-import User from '../User/User';
+import React from "react";
+import Post from "../Post/Post";
+import User from "../User/User";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home">
+    <div className="home ">
       <div className="homeleft">
         <User
           userId={"user._id"}
@@ -20,7 +20,9 @@ const Home = () => {
         <User
           userId={"user._id"}
           name={"Chethan"}
-          dp={"https://th.bing.com/th/id/OIP.LBPVUEMkAvRxo4ck7aXEJQHaE8?w=252&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"}
+          dp={
+            "https://th.bing.com/th/id/OIP.LBPVUEMkAvRxo4ck7aXEJQHaE8?w=252&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+          }
         />
         <User
           userId={"user._id"}
@@ -29,12 +31,17 @@ const Home = () => {
         />
       </div>
       <div className="homeright">
-        <Post postImage="https://cdn.unenvironment.org/2022-03/field-ge4d2466da_1920.jpg" ownName="Abhishek"
-        caption={"This is sample post of nature"}
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+        <Post
+          postImage="https://cdn.unenvironment.org/2022-03/field-ge4d2466da_1920.jpg"
+          ownName="Abhishek"
+          caption={"This is sample post of nature"}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
